6ac05b8e0b2c7219e74061210cab142bf9ec4aaa
wevre <6877948+wevre@users.noreply.github.com>
2020-12-28 14:53:37 -0700

    comment, name cleanup

--- src/advent_of_code/2020/day_1.clj
+++ src/advent_of_code/2020/day_1.clj
@@ -2,27 +2,24 @@
   (:require [clojure.string :as str]
             [clojure.math.combinatorics :as combo]))
 
-;; A brute-corce approach, trying every possible combination. There are only 
-;; about 200 numbers in the input file, so this works okay. Another approach 
-;; that works for the 2-number sum, is to sort the numbers, then search from
-;; each end until we find the combination that matches the goal.
+; A brute-corce approach, trying every possible combination. There are only 
+; about 200 numbers in the input file, so this works okay.
 
-(defn combo-sum [xs sum cnt]
+(defn combo-sum [sum cnt xs]
   (->> (combo/combinations xs cnt)
        (filter #(= sum (apply + %)))
        first))
 
-(defn puzzle [in sum cnt]
-  (let [xs (->> in
-                str/split-lines
-                (map #(Integer/parseInt %)))]
-    (apply * (combo-sum xs sum cnt))))
+(defn puzzle [sum cnt input]
+  (->> (str/split-lines input)
+       (map #(Integer/parseInt %))
+       (combo-sum sum cnt)
+       (apply *)))
 
 (comment
+  (puzzle 2020 2 (slurp "input/2020/1-expense_report.txt"))
+
+  (puzzle 2020 3 (slurp "input/2020/1-expense_report.txt"))
+
   (let [input "1721\n979\n366\n299\n675\n1456"]
-    [(puzzle input 2020 2)
-     (puzzle input 2020 3)])
-
-  (puzzle (slurp "input/2020/1-expense_report.txt") 2020 2)
-
-  (puzzle (slurp "input/2020/1-expense_report.txt") 2020 3))
+    [(puzzle 2020 2 input) (puzzle 2020 3 input)]))
