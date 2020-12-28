5586aedf1140168c66d0dc24bd09251bceee0234
wevre <6877948+wevre@users.noreply.github.com>
2020-12-28 16:30:07 -0700

    arg names/order

--- src/advent_of_code/2020/day_6.clj
+++ src/advent_of_code/2020/day_6.clj
@@ -2,18 +2,17 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-(defn puzzle [in rf]
-  (->> (str/split in #"\n\n")
+(defn puzzle [rf input]
+  (->> (str/split input #"\n\n")
        (map str/split-lines)
        (map (fn [s] (reduce rf (map set s))))
        (map count)
        (reduce +)))
 
 (comment
-  (puzzle (slurp "input/2020/6-customs.txt") set/union)
-  (puzzle (slurp "input/2020/6-customs.txt") set/intersection)
-  
+  (puzzle set/union (slurp "input/2020/6-customs.txt"))
+  (puzzle set/intersection (slurp "input/2020/6-customs.txt"))
+
   (let [input "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"]
-    [(puzzle input set/union)
-     (puzzle input set/intersection)])
-  )
+    [(puzzle set/union input)
+     (puzzle set/intersection input)]))
