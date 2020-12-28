223b631b1550027f3e72978b2dde7e4062453b28
wevre <6877948+wevre@users.noreply.github.com>
2020-12-28 16:09:55 -0700

    comment/arg-name/edn cleanup

--- src/advent_of_code/2020/day_4.clj
+++ src/advent_of_code/2020/day_4.clj
@@ -1,19 +1,20 @@
 (ns advent-of-code.2020.day-4
   (:require [clojure.string :as str]
-            [clojure.set]))
+            [clojure.set]
+            [clojure.edn :as edn]))
 
 (defn keys-present? [pp]
   (clojure.set/subset? #{:hgt :pid :byr :eyr :iyr :ecl :hcl} (set (keys pp))))
 
 (defn valid? [{:keys [byr iyr eyr hgt hcl ecl pid]}]
   (and
-   (<= 1920 (Integer/parseInt byr) 2002)
-   (<= 2010 (Integer/parseInt iyr) 2020)
-   (<= 2020 (Integer/parseInt eyr) 2030)
-   (when-let [[_ n unit] (re-find #"(\d+)(cm|in)" hgt)]
+   (<= 1920 (edn/read-string byr) 2002)
+   (<= 2010 (edn/read-string iyr) 2020)
+   (<= 2020 (edn/read-string eyr) 2030)
+   (when-let [[_ n unit] (re-matches #"(\d+)(cm|in)" hgt)]
      (case unit
-       "cm" (<= 150 (Integer/parseInt n) 193)
-       "in" (<= 59 (Integer/parseInt n) 76)
+       "cm" (<= 150 (edn/read-string n) 193)
+       "in" (<= 59 (edn/read-string n) 76)
        false))
    (re-matches #"#[0-9a-f]{6}" hcl)
    (re-matches #"\d{9}" pid)
@@ -22,16 +23,16 @@
 (defn parse [entries]
   (into {} (map (fn [[k w]] [(keyword k) w]) (partition 2 entries))))
 
-(defn puzzle [in pred]
-  (let [passports (->> (str/split in #"\n\n")
+(defn puzzle [pred input]
+  (let [passports (->> (str/split input #"\n\n")
                        (map #(re-seq #"[\w#]+" %))
                        (map parse))]
     (count (filter pred passports))))
 
 (comment
   
-  (puzzle (slurp "input/2020/4-passports.txt") keys-present?)
-  (puzzle (slurp "input/2020/4-passports.txt") (every-pred keys-present? valid?))
+  (puzzle keys-present? (slurp "input/2020/4-passports.txt"))
+  (puzzle (every-pred keys-present? valid?) (slurp "input/2020/4-passports.txt"))
   
   (let [input "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -46,6 +47,6 @@ hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in"]
-    [(puzzle input keys-present?)
-     (puzzle input (every-pred keys-present? valid?))])
+    [(puzzle keys-present? input)
+     (puzzle (every-pred keys-present? valid?) input)])
   )
