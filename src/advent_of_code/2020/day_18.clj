7661f0efa087ddaa8f1af3e597e8ada89bc9ab59
wevre <6877948+wevre@users.noreply.github.com>
2020-12-28 13:51:36 -0700

    added postwalk to Day 18

--- src/advent_of_code/2020/day_18.clj
+++ src/advent_of_code/2020/day_18.clj
@@ -1,40 +1,38 @@
 (ns advent-of-code.2020.day-18
   (:require [clojure.edn :as edn]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.walk :as walk]))
 
-(defn math-eval [[op1 op & [op2 & rest]]]
-  (if op
-    (recur (cons ((resolve op)
-                  (if (list? op1) (math-eval op1) op1)
-                  (if (list? op2) (math-eval op2) op2))
-                 rest))
-    (if (list? op1) (recur op1) op1)))
+; First version didn't do postwalk elimination of lists. Without that, the eval 
+; functions have to test every arg1 and arg2 if they are lists, and recursively 
+; evaluate them. With postwalk, however, lists are taken care of before eval 
+; ever gets called, resulting in cleaner logic.
 
-(defn adv-eval [[op1 op & [op2 & rest]]]
-  (cond
-    (= op '+) (recur (cons ((resolve op)
-                            (if (list? op1) (adv-eval op1) op1)
-                            (if (list? op2) (adv-eval op2) op2))
-                           rest))
-    (= op '*) ((resolve op) 
-               (if (list? op1) (adv-eval op1) op1) 
-               (adv-eval (cons op2 rest)))
-    :else (if (list? op1) (recur op1) op1)))
+(defn math-eval [[arg1 op & [arg2 & rest]]]
+  (if op (recur (cons ((resolve op) arg1 arg2) rest)) arg1))
 
-(defn puzzle [evaluator in]
-  (->> (str/split-lines in)
+(defn adv-eval [[arg1 op & [arg2 & rest]]]
+  (case op
+    + (recur (cons (+ arg1 arg2) rest))
+    * (* arg1 (adv-eval (cons arg2 rest)))
+    arg1))
+
+(defn calc [eval line]
+  (walk/postwalk #(if (list? %) (eval %) %) line))
+
+(defn puzzle [evaluator input]
+  (->> (str/split-lines input)
        (map #(edn/read-string (str "(" % ")")))
-       (map evaluator)
+       (map (partial calc evaluator))
        (reduce +)))
 
 (comment
   (puzzle math-eval (slurp "input/2020/18-math.txt"))
   (puzzle adv-eval (slurp "input/2020/18-math.txt"))
-    
-  (map (juxt math-eval adv-eval) 
+
+  (map (juxt (partial calc math-eval) (partial calc adv-eval))
        '((1 + (2 * 3) + (4 * (5 + 6)))
          (2 * 3 + (4 * 5))
          (5 + (8 * 3 + 9 + 3 * 4 * 3))
          (5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)))
-         (((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2)))
-  )
+         (((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2))))
