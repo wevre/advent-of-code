52a7bac0e465888f4cba61ff0045373cc311f586
wevre <6877948+wevre@users.noreply.github.com>
2020-12-27 18:49:52 -0700

    cleanup on logic, bindings

--- src/advent_of_code/2020/day_16.clj
+++ src/advent_of_code/2020/day_16.clj
@@ -15,41 +15,49 @@
 
 (defn parse-fields 
   "Parses rules for a single ticket field, returning a vector of the form
-   [<field-name> <set of valid numbers>]"
+   [<field-name> <set of valid values]"
   [line]
-  (let [[field ranges] (str/split line #":")
-        ranges (partition 2 (map edn/read-string (re-seq #"\d+" ranges)))]
-    [field (reduce (fn [s [x y]] (into s (range x (inc y)))) #{} ranges)]))
+  (let [[field ranges] (str/split line #":")]
+    [field (->> (re-seq #"\d+" ranges)
+                (map edn/read-string)
+                (partition 2)
+                (reduce (fn [s [x y]] (into s (range x (inc y)))) #{}))]))
 
-(defn unwind [m]
-  (loop [fin {} pnd m]
-    (if-let [[x s] (first (filter #(= 1 (count (second %))) pnd))]
-      (recur (conj fin [x (first s)]) 
-             (reduce-kv (fn [m k v] (assoc m k (disj v (first s)))) 
-                        {} 
-                        pnd))
+(defn deduce 
+  "Input is a map of {<position> <set of valid fields for that position>}.
+   Finds a position that only has 1 valid field, adds it to the final map, and
+   removes it from all other positions. Rinse and repeat."
+  [inp]
+  (loop [fin {} inp inp]
+    (if-let [[pos fields] (first (filter #(= 1 (count (second %))) inp))]
+      (let [field (first fields)]
+        (recur (conj fin [pos field]) 
+               (reduce-kv (fn [m k v] (assoc m k (disj v field))) {} inp)))
       fin)))
 
 (defn puzzle2 [in]
   (let [[s1 s2 s3] (str/split in #"\n\n")
+        ticket (mapv edn/read-string (re-seq #"\d+" s2))
         ranges (into {} (map parse-fields (str/split-lines s1)))
-        valid (reduce set/union (vals ranges))
         nearby (->> (drop 1 (str/split-lines s3))
                     (map #(edn/read-string (str "[" % "]")))
-                    (filter #(every? valid %)))
-        by-field (apply map vector nearby)
-        potents (reduce (fn [acc [k v]] (update acc k #(if % (conj % v) #{v})))
-                        {}
-                        (for [[i f] (map-indexed vector by-field)
-                              [k v] ranges
-                              :when (every? v f)]
-                          [i k]))
-        fields (unwind potents)
-        depart-fields (reduce-kv (fn [l k v] (if (str/starts-with? v "departure") (conj l k) l)) [] fields)
-        ticket (mapv edn/read-string (re-seq #"\d+" s2))]
-    (reduce * (map ticket depart-fields))))
-
+                    (filter #(every? (reduce set/union (vals ranges)) %))
+                    (apply map vector)
+                    (map-indexed vector)
+                    (into {}))]
+    nearby #_
+    ; `nearby` is a list with an entry of `[pos [val1, val2, ...]]` for each
+    ; position, and with invalid tickets filtered out.
+    (->> (for [[pos vals] nearby [fld valid?] ranges 
+               :when (every? valid? vals)] 
+           [pos fld])
+         (group-by first)
+         (reduce-kv (fn [m k v] (assoc m k (set (map second v)))) {})
+         deduce
+         (filter #(str/starts-with? (second %) "departure"))
+         (map first)
+         (map ticket)
+         (reduce *))))
 
 (comment
-  (puzzle2 (slurp "input/2020/16-tickets.txt"))
-  )
+  (puzzle2 (slurp "input/2020/16-tickets.txt")))
