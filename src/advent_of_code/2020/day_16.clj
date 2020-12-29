fda6b2889d80d32e99ab17259063fd756641ffe2
wevre <6877948+wevre@users.noreply.github.com>
2020-12-28 22:36:23 -0700

    cleanup/commenting

--- src/advent_of_code/2020/day_16.clj
+++ src/advent_of_code/2020/day_16.clj
@@ -3,6 +3,8 @@
             [clojure.edn :as edn]
             [clojure.set :as set]))
 
+;; --- Day 16: Ticket Translation ---
+
 (defn parse-rules
   "Parses rules for a single ticket field, returning a vector of the form
    [<field-name> <set of valid values>]"
@@ -23,32 +25,33 @@
 (comment
   (puzzle1 (slurp "input/2020/16-tickets.txt")))
 
+(defn matching-fields [ranges vals]
+  (into #{} (keep (fn [[fld valid?]] (when (every? valid? vals) fld))) ranges))
+
 (defn puzzle2 [in]
   (let [[s1 s2 s3] (str/split in #"\n\n")
         ticket (mapv edn/read-string (re-seq #"\d+" s2))
         ranges (map parse-rules (str/split-lines s1))
         all-valid (reduce set/union (map second ranges))
-        valid-tickets (->> (drop 1 (str/split-lines s3))
-                           (map #(edn/read-string (str "[" % "]")))
-                           (filter #(every? all-valid %)))
-        fields-at-pos (->> (apply map vector valid-tickets)
-                           (map #(keep (fn [[fld valid?]]
-                                         (when (every? valid? %) fld))
-                                       ranges))
-                           (map set))]
-    ; `fields-at-pos` is a list where the ith element is a set of fields that
-    ; match the values at the ith position. One of those will have a single 
-    ; field that matches, we move that into final, and remove it from all
-    ; the other positions, rinse and repeat.
-    (loop [final {} fields-at-pos fields-at-pos]
-      (if-let [[pos field] 
-               (first (keep-indexed (fn [i item] (when (= 1 (count item))
-                                                   [i (first item)]))
-                                    fields-at-pos))]
-        (recur (assoc final pos field) (map #(disj % field) fields-at-pos))
-        (reduce * (keep (fn [[k v]] (when (str/starts-with? v "departure")
-                                      (ticket k)))
-                        final))))))
+        matches (->> (drop 1 (str/split-lines s3))
+                     (map #(edn/read-string (str "[" % "]")))
+                     (filter #(every? all-valid %))
+                     (apply map vector)
+                     (map (partial matching-fields ranges)))]
+    ; `matches` is a list where the ith element is a set of fields that match 
+    ; the values at the ith position. One of those will have a single matching 
+    ; field, we move that into final, remove it from all the other positions, 
+    ; rinse and repeat.
+    (->> (loop [final {} matches matches]
+           (if-let [[pos field]
+                    (first (keep-indexed (fn [i item] (when (= 1 (count item))
+                                                        [i (first item)]))
+                                         matches))]
+             (recur (assoc final pos field) (map #(disj % field) matches))
+             final))
+         (keep (fn [[pos field]]
+                 (when (str/starts-with? field "departure") (ticket pos))))
+         (reduce *))))
 
 (comment
   (puzzle2 (slurp "input/2020/16-tickets.txt")))
