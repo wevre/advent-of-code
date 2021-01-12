db0913e04a5b4f8e21624881b0b708b3017968aa
wevre <6877948+wevre@users.noreply.github.com>
2021-01-12 11:09:15 -0700

    tweaks to 2015-d15

--- src/advent_of_code/2015/day_15.clj
+++ src/advent_of_code/2015/day_15.clj
@@ -2,6 +2,8 @@
   (:require [clojure.string :as str]
             [clojure.edn :as edn]))
 
+;; --- Day 15: Science for Hungry People ---
+
 (defn score [ingredients weights]
   (->> (map (fn [ps w] (map #(* % w) ps)) ingredients weights)
        (apply map +)
@@ -12,10 +14,7 @@
 (defn calories [ingredients weights]
   (reduce + (map (fn [ps w] (* (last ps) w)) ingredients weights)))
 
-(comment
-  (calories '((-1 -2 6 3 8) (2 3 -2 -1 3)) '(40 60)))
-
-(defn puzzle1 [tsps input]
+(defn puzzle [tsps pred input]
   (let [ingreds (map #(map edn/read-string (re-seq #"-?\d+" %))
                      (str/split-lines input))
         parts (for [i1 (range (inc tsps))
@@ -23,27 +22,11 @@
                     i3 (range (inc (- tsps i1 i2)))
                     :let [i4 (- tsps i1 i2 i3)]]
                 [i1 i2 i3 i4])]
-    (reduce max (map #(score ingreds %) parts))))
+    (reduce max (map #(score ingreds %) (filter #(pred ingreds %) parts)))))
 
 (comment
-  (let [input (slurp "input/2015/15-ingredients.txt")] (puzzle1 100 input))
+  (let [input (slurp "input/2015/15-ingredients.txt")]
+    (puzzle 100 (constantly true) input))
 
-  (let [input "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
-Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"] (puzzle1 100 input)))
-
-(defn puzzle2 [tsps input]
-  (let [ingreds (map #(map edn/read-string (re-seq #"-?\d+" %))
-                     (str/split-lines input))
-        parts (for [i1 (range (inc tsps))
-                    i2 (range (inc (- tsps i1)))
-                    i3 (range (inc (- tsps i1 i2)))
-                    :let [i4 (- tsps i1 i2 i3)]
-                    :when (= 500 (calories ingreds [i1 i2 i3 i4]))]
-                [i1 i2 i3 i4])]
-    (reduce max (map #(score ingreds %) parts))))
-
-(comment
-  (let [input (slurp "input/2015/15-ingredients.txt")] (puzzle2 100 input))
-  
-  (let [input "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
-Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"] (puzzle2 100 input)))
+  (let [input (slurp "input/2015/15-ingredients.txt")]
+    (puzzle 100 (fn [is w] (= 500 (calories is w))) input)))
