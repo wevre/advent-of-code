5ad70178729b0ea8e486eb59d00c99c0217686d1
wevre <6877948+wevre@users.noreply.github.com>
2021-01-13 09:23:12 -0700

    cleanup to 2015-d17

--- src/advent_of_code/2015/day_17.clj
+++ src/advent_of_code/2015/day_17.clj
@@ -1,15 +1,14 @@
 (ns advent-of-code.2015.day-17
-  (:require [clojure.string :as str]
-            [clojure.edn :as edn]
+  (:require [clojure.edn :as edn]
             [clojure.math.combinatorics :as combo]))
 
 ;; --- Day 17: No Such Thing as Too Much ---
 
 (defn containers [vol input]
-  (let [cs (map edn/read-string (re-seq #"\d+" input))]
-    (->> (map-indexed vector cs)
-         (combo/subsets)
-         (filter #(= vol (apply + (map second %)))))))
+  (->> (map edn/read-string (re-seq #"\d+" input))
+       (map-indexed vector)
+       (combo/subsets)
+       (filter #(= vol (apply + (map second %))))))
 
 (defn puzzle1 [vol input]
   (count (containers vol input)))
