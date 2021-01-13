977211945ac5e39649404611ae54185fac1aa764
wevre <6877948+wevre@users.noreply.github.com>
2021-01-13 10:51:20 -0700

    added 2015-d18

---
+++ src/advent_of_code/2015/day_18.clj
@@ -0,0 +1,39 @@
+(ns advent-of-code.2015.day-18
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
+
+;; --- Day 18: Like a GIF For Your Yard ---
+
+(defn parse [lines]
+  (reduce (fn [res [r l]]
+            (into res (keep-indexed (fn [c v] (when (= \# v) [r c])) l)))
+          #{}
+          (map-indexed vector lines)))
+
+(defn neighbors [cell]
+  (->> (combo/selections [-1 0 1] (count cell))
+       (remove #(apply = 0 %))
+       (map #(map + cell %))))
+
+(defn step [wid keep-on grid]
+  (into keep-on
+        (set (for [[[r c :as loc] n] (frequencies (mapcat neighbors grid))
+                   :when (or (= 3 n) (and (grid loc) (= 2 n)))
+                   :when (and (< -1 r wid) (< -1 c wid))]
+               loc))))
+
+(defn puzzle [cycles wid keep-on input]
+  (->> (str/split-lines input)
+       parse
+       (into keep-on)
+       (iterate (partial step wid keep-on))
+       (drop cycles)
+       first
+       count))
+
+(comment
+  (let [input (slurp "input/2015/18-lights.txt")
+        wid 100
+        cycles 100
+        keep-on #_#{} #{[0 0] [0 99] [99 0] [99 99]}]
+    (puzzle cycles wid keep-on input)))
