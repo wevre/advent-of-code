c98f49dde4092ea79c71c9790b6ddb311fdc616d
wevre <6877948+wevre@users.noreply.github.com>
2021-12-24 08:36:46 -0700

    day 23 complete

--- src/advent_of_code/2015/day_20.clj
+++ src/advent_of_code/2015/day_20.clj
@@ -1,8 +1,45 @@
 (ns advent-of-code.2015.day-20)
 
-;;; Using Euler's recurrence for sums of divisors, with pentagonal numbers.
+;;;; --- Day 20: Infinite Elves and Infinite Houses ---
+;;;; https://adventofcode.com/2015/day/20
 
-;; I love this code, it is very math-nerd-ical. But it is slow.
+;;; Use a really large array and deliver presents brute force.
+
+;; Not as elegant of code. Uses mutable java array with side-effects. Using `do`
+;; and `doseq` is typically a code smell. At least the mutations are all
+;; self-contained within the `solve` function.
+
+(def house-count 1000000)
+(def target 33100000)
+
+(defn solve [elf-limit per-elf]
+  (let [houses (long-array house-count 10)
+        elf-range (if elf-limit (range elf-limit) (range))]
+    (loop [elf 2]
+      (if (< elf house-count)
+        (do
+          (doseq [house (map #(* (inc %) elf) elf-range)
+                  :while (< house house-count)
+                  :let [val (+ (aget houses (dec house)) (* per-elf elf))]]
+            (aset houses (dec house) val))
+          (if (<= target (aget houses (dec elf)))
+            elf
+            (recur (inc elf))))
+        :fail))))
+
+(comment
+  ;; part 1 -- ~85s
+  (time
+   (solve nil 10))   ; 776160
+
+  ;; part 2 -- ~27s
+  (time
+   (solve 50 11))   ; 786240
+  )
+
+;;; Use Euler's recurrence for sums of divisors, with pentagonal numbers.
+
+;; I love this code, it is very math-nerd-ical. But it is also slow.
 
 (defn make-second-order [x d]
   (map first (iterate (fn [[a b]] [(+ a b) (+ b d)]) [x (+ x d)])))
@@ -20,49 +57,9 @@
   )
 
 (comment
-  ;; part 1  -- takes over 9 minutes
+  ;; part 1  -- over 9 minutes
   (time
    (->> (map sigma (range))
         (take-while #(< % 3310000))
         count))
-  )
-
-;;; Using a really large array and loading up presents brute force.
-
-(comment
-  ;; part 1 -- takes 2.5 minutes
-  (do
-    (def count 1000000)
-    (def houses (long-array count 10))
-    (def target 33100000)
-    (time
-     (loop [elf 2]
-       (if (< elf count)
-         (do
-           (doseq [house (map #(* (inc %) elf) (range))
-                   :while (< house count)
-                   :let [val (+ (aget houses (dec house)) (* 10 elf))]]
-             (aset houses (dec house) val))
-           (if (<= target (aget houses (dec elf)))
-             elf
-             (recur (inc elf))))
-         :fail))))
-
-  ;; part 2 -- takes 1.5 seconds
-  (do
-    (def count 2000000)
-    (def houses (long-array count 10))
-    (def target 33100000)
-    (time
-     (loop [elf 2]
-       (if (< elf count)
-         (do
-           (doseq [house (map #(* % elf) (range 1 51))
-                   :when (< house count)
-                   :let [val (+ (aget houses (dec house)) (* 11 elf))]]
-             (aset houses (dec house) val))
-           (if (<= target (aget houses (dec elf)))
-             elf
-             (recur (inc elf))))
-         :fail))))
   )
--- src/advent_of_code/2021/day_23.clj
+++ src/advent_of_code/2021/day_23.clj
@@ -1,82 +1,136 @@
 (ns advent-of-code.2021.day-23
-  (:require [clojure.string :as str]
-            [clojure.java.math :as math]))
+  (:require [clojure.data.priority-map :refer [priority-map-keyfn]]
+            [clojure.java.math :as math]
+            [clojure.string :as str]))
 
-(defn parse-input [s]
-  {:cost 0
-   :burrow (->> (str/split-lines s)
-                (map-indexed vector)
-                (mapcat (fn [[r l]]
-                          (map-indexed (fn [c v] [[r c] v]) l)))
-                (into {}))})
+;;;; --- Day 23: Amphipod ---
+;;;; https://adventofcode.com/2021/day/23
 
-(def energy {\A 1 \B 10 \C 100 \D 1000})
+;;; Parameters
 
-(def room-column {\A 3 \B 5 \C 7 \D 9})
+(def room-capacity 2)
+(def room-col {:A 2 :B 4 :C 6 :D 8})
 
-(defn valid-move [[r-from c-from] [r-dest c-dest] pod burrow]
-  (cond
-    (= r-from r-dest 1) false   ; no wandering in hallway
-    (and (= 3 r-from) (not= \. (burrow [2 c-from]))) false   ; blocked room
-    (and (= 3 r-from) (= c-from (room-column pod))) false   ; stay put!
-    (and (= 2 r-from) (= c-from (room-column pod)) (= pod (burrow [3 c-from]))) false   ; done!
-    (and (= 2 r-dest) (= \. (burrow [3 c-dest]))) false   ; go all the way back
-    (and (= 2 r-dest) (not= pod (burrow [3 c-dest]))) false   ; stranger
-    (and (<= 2 r-dest 3) (not= (room-column pod) c-dest)) false   ; not your room
-    (and (= 1 r-dest) (#{3 5 7 9} c-dest)) false   ; don't linger in front of room
-    :else (every? #{\.} (map #(burrow [1 %])
-                             (if (< c-from c-dest)
-                               (range (inc c-from) (inc c-dest))
-                               (range c-dest c-from)))))
-  )
-
-(defn distance [[r-from c-from] [r-dest c-dest]]
-  (+
-   (if (and (<= 2 r-from 3) (<= 2 r-dest 3))
-     (+ (dec r-from) (dec r-dest))
-     (math/abs (- r-from r-dest)))
-   (math/abs (- c-from c-dest))))
-
-(defn moves [{burrow :burrow :as state}]
-  (for [[from pod] burrow :when (#{\A \B \C \D} pod)
-        dest (keys (filter #(= \. (val %)) burrow))
-        :when (valid-move from dest pod burrow)
-        :let [distance (distance from dest)]]
-    (-> state
-        (assoc-in [:burrow dest] pod)
-        (assoc-in [:burrow from] \.)
-        (update :cost + (* distance (energy pod))))))
-
-(defn organized? [{burrow :burrow}]
-  (every? true?
-          (for [[pod col] room-column]
-            (= pod (burrow [2 col]) (burrow [3 col])))))
-
-(def organize
-  (memoize
-   (fn [state]
-     (if (organized? state)
-       (:cost state)
-      (reduce min ##Inf (for [move (moves state)] (organize move)))))))
-
-(defn print-state [{:keys [cost burrow]}]
-  (println "Cost:" cost)
-  (doseq [r (range 5)]
-    (doseq [c (range 13)
-            :let [v (burrow [r c])]
-            :when (some? v)]
-      (print (burrow [r c])))
+(defn print-burrow [{:keys [hall rooms]}]
+  (doseq [x (range 11)]
+    (print (if (contains? hall x) (or (some-> x hall name) \·) " ")))
+  (println)
+  (doseq [r (range room-capacity)]
+    (print "  ")
+    (apply print (map #(or (some-> (get-in rooms [% r] nil) name) \·) [:A :B :C :D]))
     (println)))
 
-(comment
-  (let [input (parse-input "#############
-#...........#
-###A#B#C#D###
-  #A#B#C#D#
-  #########")]
-    (organized? input))
+;;; Finding moves
 
+(def energy {:A 1 :B 10 :C 100 :D 1000})
+
+(defn hall-clear? [c1 c2 hall]
+  (let [[c1 c2] (if (< c1 c2) [c1 c2] [c2 c1])]
+    (->> hall
+         (filter (fn [[col _]] (< c1 col c2)))
+         (every? (fn [[_ pod]] (nil? pod))))))
+
+(defn room-clear? [pod rooms] (every? #(or (nil? %) (= pod %)) (rooms pod)))
+
+(defn moves [{:keys [rooms hall] :as burrow}]
+  (let [move-outs
+        (for [[k room] rooms
+              :let [col (room-col k)
+                    [vac [f & fs]] (split-with nil? room)]
+              :when f
+              :let [dest (room-col f)]
+              :when (or (not= col dest) (apply not= f fs))]
+          {:from-col col :out-dist (inc (count vac)) :pod f :vacate [:rooms k (count vac)]})
+        move-ins
+        (for [[col f] hall :when f] {:from-col col :pod f :vacate [:hall col]})]
+    (concat
+     ;; try each move-out to hallway spots
+     (for [{:keys [from-col out-dist pod vacate]} move-outs
+           to-col (keep (fn [[c f]] (when-not f c)) hall)
+           :when (hall-clear? from-col to-col hall)]
+       {:move/burrow (-> burrow
+                         (assoc-in [:hall to-col] pod)
+                         (assoc-in vacate nil))
+        :move/cost (* (energy pod) (+ out-dist (math/abs (- from-col to-col))))})
+     ;; try pods to their home
+     (for [{:keys [from-col out-dist pod vacate] :or {out-dist 0}} (concat move-ins move-outs)
+           :let [to-col (room-col pod)
+                 in-dist (count (filter nil? (rooms pod)))]
+           :when (and (hall-clear? from-col to-col hall) (room-clear? pod rooms))]
+       {:move/burrow (-> burrow
+                         (assoc-in [:rooms pod (dec in-dist)] pod)
+                         (assoc-in vacate nil))
+        :move/cost (* (energy pod) (+ out-dist in-dist (math/abs (- from-col to-col))))}))))
+
+(comment
+  (let [burrow {:hall {0 nil 1 :B 3 nil 5 nil 7 :D 9 :C 10 nil}
+                :rooms {:A [:A :B] :B [nil :B] :C [:C :A] :D [nil :D]}}]
+    (print-burrow burrow)
+    (doseq [move (moves burrow)]
+      (println "Cost:" (:move/cost move))
+      (print-burrow (:move/burrow move))))
+  )
+
+;;; Costs
+
+(defn room-penalty [[k room]]
+  (let [room (remove nil? room)]
+    (->> (map room-col room)
+         (map #(math/abs (- (room-col k) %)))
+         (map #(if (zero? %) 0 (+ 2 %)))
+         (map * (map energy room))
+         (reduce +))))
+
+(defn hall-penalty [[col pod]]
+  (if pod
+    (let [dist (math/abs (- col (room-col pod)))]
+      (* (energy pod) (+ 1 dist)))
+    0))
+
+(defn penalty [burrow]
+  (+
+   (reduce + (map hall-penalty (:hall burrow)))
+   (reduce + (map room-penalty (:rooms burrow)))))
+
+(comment
+  (let [burrow {:hall {0 nil 1 nil 3 nil 5 nil 7 nil 9 nil 10 nil}
+                :rooms {:A [:A :B] :B [:C :D] :C [:C :A] :D [:D :B]}}]
+    (println "penalty:" (penalty burrow))
+    (print-burrow burrow))
+  )
+
+(defn update-costs [{node-cost :node/cost}]
+  (fn [costs {:move/keys [burrow cost]}]
+    (if (contains? costs burrow)
+      (update-in costs [burrow :node/cost] (fnil min ##Inf) (+ node-cost cost))
+      (assoc costs burrow {:node/cost (+ node-cost cost) :node/penalty (penalty burrow)}))))
+
+;;; Solve
+
+(defn organize [start]
+  (let [end (into {} (map (juxt identity #(vec (repeat room-capacity %))) [:A :B :C :D]))]
+    (loop [costs (priority-map-keyfn #(+ (:node/cost %) (:node/penalty %))
+                                     start
+                                     {:node/cost 0 :node/penalty (penalty start)})
+           visited #{}]
+      (let [[burrow cost] (peek costs)]
+        (if (= (:rooms burrow) end)
+          cost
+          (let [moves (->> burrow moves (remove #(visited (:move/burrow %))))]
+            (recur (reduce (update-costs cost) (pop costs) moves)
+                   (conj visited burrow))))))))
+
+(comment
+  ;; part 1 -- ~3s
   (time
-   (let [input (parse-input (slurp "input/2021/23-amphipods.txt"))]
-     (organize input)))
+   (let [burrow {:hall {0 nil 1 nil 3 nil 5 nil 7 nil 9 nil 10 nil}
+                 :rooms {:A [:A :B] :B [:C :D] :C [:C :A] :D [:D :B]}}]
+     (organize burrow)))   ; 13066
+
+  ;; part 2 -- ~9s
+  (time
+   (with-redefs [room-capacity 4]
+     (let [burrow {:hall {0 nil 1 nil 3 nil 5 nil 7 nil 9 nil 10 nil}
+                   :rooms {:A [:A :D :D :B] :B [:C :C :B :D] :C [:C :B :A :A] :D [:D :A :C :B]}}]
+       (organize burrow))))
   )
