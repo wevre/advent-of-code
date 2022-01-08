0afceec5b71f6b6bee3c04f7cec31b6df152869c
wevre <6877948+wevre@users.noreply.github.com>
2022-01-08 13:30:05 -0700

    switched from cond to case

--- src/advent_of_code/2015/day_23.clj
+++ src/advent_of_code/2015/day_23.clj
@@ -5,19 +5,19 @@
 (defn parse-input [s]
   (vec
    (for [line (str/split-lines s)]
-     (edn/read-string (str "(" line ")")))))
+     (edn/read-string (str "(:" line ")")))))
 
 (defn execute [program registers]
   (loop [ptr 0 registers registers]
     (let [[op c d] (nth program ptr '(hlt))]
-      (cond
-        (= op 'hlf) (recur (inc ptr) (update registers c quot 2))
-        (= op 'tpl) (recur (inc ptr) (update registers c * 3))
-        (= op 'inc) (recur (inc ptr) (update registers c inc))
-        (= op 'jmp) (recur (+ ptr c) registers)
-        (= op 'jie) (recur (+ ptr (if (even? (registers c)) d 1)) registers)
-        (= op 'jio) (recur (+ ptr (if (= 1 (registers c)) d 1)) registers)
-        :else registers))))
+      (case op
+        :hlf (recur (inc ptr) (update registers c quot 2))
+        :tpl (recur (inc ptr) (update registers c * 3))
+        :inc (recur (inc ptr) (update registers c inc))
+        :jmp (recur (+ ptr c) registers)
+        :jie (recur (+ ptr (if (even? (registers c)) d 1)) registers)
+        :jio (recur (+ ptr (if (= 1 (registers c)) d 1)) registers)
+        registers))))
 
 (comment
   ;; part 1
