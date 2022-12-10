b76be21041b86d6333b67c354b3e5b3de6b870b7
wevre <6877948+wevre@users.noreply.github.com>
2022-12-10 11:28:34 -0700

    some refactoring

--- src/advent_of_code/2022/day_10_signals.clj
+++ src/advent_of_code/2022/day_10_signals.clj
@@ -1,26 +1,24 @@
 (ns advent-of-code.2022.day-10-signals
-  (:require [clojure.edn :as edn]
+  (:require [advent-of-code.common :as common]
             [clojure.string :as str]))
 
-(defn parse [input]
-  (->> input
-       str/split-lines
-       (map #(edn/read-string (str "[" % "]")))
-       #_(mapcat #(if (= 'addx (first %)) [0 (second %)] [0]))
-       (mapcat #(assoc % 0 0))   ; borrowed trick from @tschady
-       (reductions + 1)))
+(defn parse [numbering input]
+  ;; Trick from @tschady to directly replace instruction with 0. My original
+  ;; version tested for `'addx` and fed [0 x] or [0] into `mapcat`.
+  (->> (str/replace input #"addx|noop" "0")
+       common/parse-longs
+       (reductions + 1)
+       (map vector numbering)))
 
 (comment
   ;; puzzle 1
-  (->> (parse (slurp "input/2022/10-signals.txt"))
-       (map vector (drop 1 (range)))
+  (->> (parse (drop 1 (range)) (slurp "input/2022/10-signals.txt"))
        (filter #(#{20 60 100 140 180 220} (first %)))
        (map #(apply * %))
        (apply +))   ; => 17020
 
   ;; puzzle 2
-  (->> (parse (slurp "input/2022/10-signals.txt"))
-       (map vector (cycle (range 40)))
+  (->> (parse (cycle (range 40)) (slurp "input/2022/10-signals.txt"))
        (map #(apply - %))
        (map #({-1 \# 0 \# +1 \#} % \.))
        (partition 40)
