fba55e95a1e643d75b65ca0dffd90c0e8a36e963
wevre <6877948+wevre@users.noreply.github.com>
2022-12-10 10:48:31 -0700

    year 2022 day 10

---
+++ src/advent_of_code/2022/day_10_signals.clj
@@ -0,0 +1,26 @@
+(ns advent-of-code.2022.day-10-signals
+  (:require [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(edn/read-string (str "[" % "]")))
+       (mapcat #(if (= 'addx (first %)) [0 (second %)] [0]))
+       (reductions + 1)))
+
+(comment
+  ;; puzzle 1
+  (->> (parse (slurp "input/2022/10-signals.txt"))
+       (map vector (drop 1 (range)))
+       (filter #(#{20 60 100 140 180 220} (first %)))
+       (map #(apply * %))
+       (apply +))   ; => 17020
+
+  ;; puzzle 2
+  (->> (parse (slurp "input/2022/10-signals.txt"))
+       (map vector (cycle (range 40)))
+       (map #(if (<= -1 (apply - %) 1) \# \.))
+       (partition 40)
+       (map #(apply str %)))   ; => RLEZFLGE
+  )
