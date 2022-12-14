4b22cda459a2cf319dcaea8dbbd01ca678d86e0a
wevre <6877948+wevre@users.noreply.github.com>
2022-12-14 07:54:25 -0700

    debug cleanup

--- src/advent_of_code/2022/day_14_sand.clj
+++ src/advent_of_code/2022/day_14_sand.clj
@@ -4,72 +4,49 @@
 
 ;; 2022-12-13 10:56
 ;;    So sloppy and crude, but I don't care. I got the stars and now I can go
-;;    and get some rest for snowboarding tomorrow!
+;;    and get some rest for snowboarding tomorrow! Since I already had a
+;;    function `points-along` from prior years, I contorted my input into
+;;    something I could feed into that function in order to find rocks.
+;; 2012-12-14 06:56
+;;    Did some cleanup, mostly to function signatures, but didn't change the
+;;    logic. I now basically have only the one function, `cascade`. I pulled
+;;    `blocked?` inside since it depends on whether or not there is a `floor?`.
+;;    I decided to go with nesting `if`'s instead of nesting `cond`'s because
+;;    I'm sort of allergic to nested `cond`'s and easier to wrap the branch of
+;;    an `if` in a `let` to get some needed values.
+
+(defn- get-points [lines]
+  (reduce (fn [acc [a b]] (into acc (common/points-along a b))) #{} lines))
+
+(defn- parse-line [line]
+  (->> line common/parse-longs (partition 2) (partition 2 1) get-points))
 
 (defn get-rocks [input]
-  (let [get-points (fn [lines]
-                     (reduce (fn [acc [a b]]
-                               (into acc (common/points-along a b)))
-                             #{}
-                             lines))]
-    (->> input
-         str/split-lines
-         (map common/parse-longs)
-         (map #(partition 2 %))
-         (map #(partition 2 1 %))
-         (map get-points)
-         (reduce into #{}))))
+  (->> input str/split-lines (map parse-line) (reduce into #{})))
 
-(defn- blocked? [pos rocks sand]
-  (or (rocks pos) (sand pos)))
-
-(defn- floored [floor]
-  (fn [[_ y :as pos] rocks sand]
-    (or (= y floor) (blocked? pos rocks sand))))
-
-(def start [500 0])
-
-(def ^:dynamic *debug* false)
-
-(defn cascade [lowest rocks block-fn]
-  (fn [n [x y :as pos] sand]
-    (when *debug* (println "try" pos "sand is" sand))
-    (cond
-
-      (and *debug* (neg? n)) sand
-
-      (sand start) sand
-
-      (> y lowest) sand
-
-      (block-fn [x (inc y)] rocks sand)
-      (cond
-        (not (block-fn [(dec x) (inc y)] rocks sand)) (do (when *debug* (println "try left")) (recur (dec n) [(dec x) (inc y)] sand))
-        (not (block-fn [(inc x) (inc y)] rocks sand)) (do (when *debug* (println "try right")) (recur (dec n) [(inc x) (inc y)] sand))
-        :else (do (when *debug* (println "rest at" pos)) (recur (dec n) start (conj sand pos))))
-
-      :else (recur (dec n) [x (inc y)] sand)
-      ))
-  )
-
+(defn cascade [floor? rocks]
+  (let [start [500 0]
+        lowest (cond-> (reduce max (map second rocks)) floor? (+ 2))
+        blocked? (fn [[_ y :as pos] sand]
+                   (or (and floor? (= y lowest)) (rocks pos) (sand pos)))]
+    (loop [[x y :as pos] start sand #{}]
+      (if (or (sand start) (> y lowest))
+        sand                                                   ; Done
+        (let [down [x (inc y)]
+              left [(dec x) (inc y)]
+              right [(inc x) (inc y)]]
+          (if (blocked? down sand)
+            (cond
+              (not (blocked? left sand)) (recur left sand)     ; Left
+              (not (blocked? right sand)) (recur right sand)   ; Right
+              :else (recur start (conj sand pos)))             ; Rest
+            (recur down sand)))))))                            ; Down
 
 (comment
   ;; puzzle 1
-  (let [#_#_input "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9"
-        input (slurp "input/2022/14-sand.txt")
-        rocks (get-rocks input)
-        lowest (reduce max (map second rocks))
-        sand ((cascade lowest rocks blocked?) 200 start #{})]
-    (count sand)
-    )
+  (->> (slurp "input/2022/14-sand.txt") get-rocks (cascade false) count)   ; => 683
 
   ;; puzzle 2
-  (binding [*debug* false]
-    (let [#_#_input "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9"
-          input (slurp "input/2022/14-sand.txt")
-          rocks (get-rocks input)
-          lowest (reduce max (map second rocks))
-          sand ((cascade (+ lowest 2) rocks (floored (+ lowest 2))) 1000 start #{})]
-      (count sand)))
-
+  (time
+   (->> (slurp "input/2022/14-sand.txt") get-rocks (cascade true) count))   ; => 28821
   )
