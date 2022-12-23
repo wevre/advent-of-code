811fd4e1d3e4b24fdcfb81575d6cb61819d2f8aa
wevre <6877948+wevre@users.noreply.github.com>
2022-12-23 12:47:37 -0700

    Year 2022 Day 17 correct solution

--- src/advent_of_code/2022/day_17_tetris.clj
+++ src/advent_of_code/2022/day_17_tetris.clj
@@ -23,35 +23,63 @@
       (reduced p)
       ∆p)))
 
-(defn move [loc blocks h shapes [[jet i] & jets]]
-  (let [[n shape] (first shapes)]
-    (if-not shape
-      {:h h :blocks blocks :loc loc}
-      (let [loc (-> loc (blow shape jet blocks) (fall shape blocks))]
-        (if (reduced? loc)
-          (let [loc (unreduced loc)
-                ∆shape (map #(add loc %) (:blocks shape))
-                h (reduce (fn [h [_ y]] (max h y)) h ∆shape)
-                _ (when (and (= 0 (first loc)) (= \- (:name shape))) (println "i" i "n" n "h" h "loc" loc))]
-            (recur [2 (+ h 4)] (into blocks (map vector ∆shape (repeat (:name shape)))) h (rest shapes) jets))
-          (recur loc blocks h shapes jets))))))
+(defn prune [cnt h i blocks {:keys [mod-i ∆cnt ∆h]}]
+  (let [q (quot cnt ∆cnt)]
+    (if (and (= i mod-i) (< 0 q))
+      (let [cnt (- cnt (* q ∆cnt))
+            h (+ h (* q ∆h))
+            _ (println "pruning cnt" cnt "h" h "i" i mod-i)
+            _ (println "size of blocks" (count blocks))]
+        [cnt h (reduce (fn [m [k _]] (assoc m (add [0 (* q ∆h)] k) \@)) {} blocks) #_(update-keys blocks #(add [0 (* q ∆h)] %)) #_(into {} (for [x (range 7)] [[x h] \_]))])
+      [cnt h blocks])))
+
+(defn move [reset]
+  (fn [cnt loc blocks h shapes [[jet i] & jets]]
+    (let [shape (first shapes)]
+      (if-not (pos? cnt)
+        {:h h :blocks blocks}
+        (let [loc (-> loc (blow shape jet blocks) (fall shape blocks))]
+          (if (reduced? loc)
+            (let [loc (unreduced loc)
+                  ∆shape (map #(add loc %) (:blocks shape))
+                  h (reduce (fn [h [_ y]] (max h y)) h ∆shape)
+                  blocks (into blocks (map vector ∆shape (repeat (:name shape))))
+                  _ (when (= i (:mod-i reset)) (println "shape" (:name shape)))
+                  _ (when (and (= i (:mod-i reset)) (= (:loc-x reset) (first loc)) (= \- (:name shape))) (println "before i" i "cnt" cnt "h" h "loc" loc))
+                  [cnt h blocks] (prune cnt h i blocks reset)
+                  _ (when (and (= i (:mod-i reset)) (= (:loc-x reset) (first loc)) (= \- (:name shape))) (println "after i" i "cnt" cnt "h" h "blocks" (take 10 blocks)))]
+              (recur (dec cnt) [2 (+ h 4)] blocks h (rest shapes) jets))
+            (recur cnt loc blocks h shapes jets)))))))
 
 (defn printout [h blocks]
-  (doseq [y (range 60)
+  (doseq [y (range 30)
           :let [row (for [x (range 7)] (if-let [block (blocks [x (- h y)])] block \.))]]
     (println (str/join row))))
 
 (comment
-  ;; part 1 -- ~150msecs
+  ;; part 1 -- ~164msecs
   (time
-   (let [jets #_">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-         (slurp "input/2022/17-jet-pattern.txt")
+   (let [jets #_">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>" (slurp "input/2022/17-jet-pattern.txt")
+         reset #_{:mod-i 37 :∆cnt 35 :∆h 53 :loc-x 1}      {:mod-i 104 :∆cnt 1735 :∆h 2781 :loc-x 0}
          p [2 3]
          blocks (into {} (for [x (range 7)] [[x -1] \_]))
-         shapes (->> shapes cycle (map vector (range)) (take 3500))
+         shapes (cycle shapes)
          jets (cycle (map vector jets (range)))
-         {:keys [h blocks loc]} (move p blocks 0 shapes jets)]
-     (printout (+ h 3) blocks)))
+         {:keys [h blocks]} ((move reset) 2022 p blocks 0 shapes jets)]
+     (println "tower height" (inc h))
+     (printout (+ h 3) blocks)))   ; => 3206
+
+  ;; part 2 -- ~175msecs
+  (time
+   (let [jets #_">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>" (slurp "input/2022/17-jet-pattern.txt")
+         reset #_{:mod-i 37 :∆cnt 35 :∆h 53 :loc-x 1}      {:mod-i 104 :∆cnt 1735 :∆h 2781 :loc-x 0}
+         p [2 3]
+         blocks (into {} (for [x (range 7)] [[x -1] \_]))
+         shapes (cycle shapes)
+         jets (cycle (map vector jets (range)))
+         {:keys [h blocks]} ((move reset) 1000000000000 p blocks 0 shapes jets)]
+     (println "tower height" (inc h))
+     (printout (+ h 3) blocks)))   ; => 1602881844347
   )
 
 ;; 2022-12-23
@@ -73,3 +101,7 @@
 ;;    also have a `reset` map so that if we hit the start of a cycle, it
 ;;    can fast-forward shapes and heights until it comes closer to the desired
 ;;    n, and then we'll just do normal processing from that point forward.
+;;
+;;   Something is not right with my choice of reset for the puzzle input. I need
+;;   to re-examine that. It works on the sample puzzle, because only pulling
+;;   2022 shapes we never hit a reset.
