e85a1b7a773f6949b35b64bc50f424de8cec9680
wevre <6877948+wevre@users.noreply.github.com>
2023-12-01 23:16:05 -0700

    slight shortening

--- src/advent_of_code/2023/day_02_games.clj
+++ src/advent_of_code/2023/day_02_games.clj
@@ -2,17 +2,14 @@
   (:require [clojure.string :as str]))
 
 (defn parse-info [s]
-  (let [[game-id & info] (re-seq #"\d+|blue|green|red" s)
-        max's (reduce (fn [m [n k]] (update m (keyword k) max (parse-long n)))
-                      {:blue 0 :green 0 :red 0}
-                      (partition 2 info))]
-    (assoc max's :game (parse-long game-id))))
+  (let [[game-id & info] (re-seq #"\d+|blue|green|red" s)]
+    (reduce (fn [m [n k]] (update m (keyword k) max (parse-long n)))
+            {:blue 0 :green 0 :red 0 :game (parse-long game-id)}
+            (partition 2 info))))
 
 (defn ?possible [limits]
   (fn [game-info]
-    (->> (merge-with - limits game-info)
-         vals
-         (not-any? neg?))))
+    (->> (merge-with - limits game-info) vals (not-any? neg?))))
 
 (defn power [{:keys [blue green red]}] (* blue green red))
 
@@ -21,16 +18,11 @@
   (def lines (-> (slurp "input/2023/02-sample-games.txt") str/split-lines))
 
   ;; year 2023 day 01 puzzle 1
-  (->> lines
-       (map parse-info)
-       (filter (?possible {:blue 14 :green 13 :red 12}))
-       (map :game)
-       (reduce +))
+  (transduce (comp (map parse-info)
+                   (filter (?possible {:blue 14 :green 13 :red 12}))
+                   (map :game))
+             +
+             lines)
 
   ;; year 2023 day 01 puzzle 2
-  (->> lines
-       (map parse-info)
-       (map power)
-       (reduce +))
-
-  )
+  (transduce (comp (map parse-info) (map power)) + lines))
