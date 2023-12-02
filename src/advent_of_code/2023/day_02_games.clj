0c0c5019eec04b10383683f74e332e773e3195a5
wevre <6877948+wevre@users.noreply.github.com>
2023-12-02 09:10:07 -0700

    no need to parameterize limits

--- src/advent_of_code/2023/day_02_games.clj
+++ src/advent_of_code/2023/day_02_games.clj
@@ -4,11 +4,12 @@
 (defn parse-info [s]
   (let [[game-id & info] (re-seq #"\d+|blue|green|red" s)]
     (reduce (fn [m [n k]] (update m (keyword k) max (parse-long n)))
-            {:blue 0 :green 0 :red 0 :game (parse-long game-id)}
-            (partition 2 info))))
+            {:blue 0 :green 0 :red 0 :game (parse-long game-id)} (partition 2 info))))
 
-(defn ?possible [limits]
-  (fn [game-info] (->> (merge-with - limits game-info) vals (not-any? neg?))))
+(def limits {:blue 14 :green 13 :red 12})
+
+(defn ?possible [game-info]
+  (->> (merge-with - limits game-info) vals (not-any? neg?)))
 
 (defn power [{:keys [blue green red]}] (* blue green red))
 
@@ -19,9 +20,8 @@
   (def games (games<- "input/2023/02-sample-games.txt"))
 
   ;; year 2023 day 01 puzzle 1
-  (let [xform (comp (filter (?possible {:blue 14 :green 13 :red 12})) (map :game))]
-    (transduce xform + games))   ;; => 1853
+  (transduce (comp (filter ?possible) (map :game)) + games)   ;; => 1853
 
   ;; year 2023 day 01 puzzle 2
   (transduce (map power) + games)   ;; => 72706
-)
+  )
