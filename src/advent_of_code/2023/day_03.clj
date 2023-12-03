4977d1ffe0ff3fb31b242ed9d01d1e3e406fc216
wevre <6877948+wevre@users.noreply.github.com>
2023-12-03 01:00:40 -0700

    improved the commentary and function names

--- src/advent_of_code/2023/day_03.clj
+++ src/advent_of_code/2023/day_03.clj
@@ -1,42 +1,39 @@
 (ns advent-of-code.2023.day-03
   (:require [advent-of-code.common :refer [locmap<-]]))
 
-(def digits #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
+(def ?digit #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
 
-(defn ?symbol [v] (and (not= v \.) (not (digits v))))
-
-(defn symbols<- [locmap]
-  (into {} (filter #(?symbol (second %))) locmap))
+(defn ?symbol [v] (and (not= v \.) (not (?digit v))))
 
 (defn neighbors [[r c]]
   (for [dr [-1 0 1] dc [-1 0 1] :when (not= 0 dr dc)]
     [(+ r dr) (+ c dc)]))
 
 (defn adjacent-digits [locmap sloc]
-  (for [nloc (neighbors sloc) :when (digits (locmap nloc))] nloc))
+  (for [nloc (neighbors sloc) :when (?digit (locmap nloc))] nloc))
 
-(defn non-digit
+(defn find-non-digit
   "Starting at location [r c] increment or decrement c (based on op) until we
    encounter something not a digit and return c. If we run off the edge of the
-   map return n, which will be (depending on op) the outermost valid c."
+   map return n, which will be the outermost valid c (in the direction of op)."
   [locmap r c op n]
   (or
-   (->> (range)
-        (map (fn [i] [r (op c 1 i)]))
-        (drop-while (fn [loc] (digits (locmap loc))))
+   (->> (drop 1 (range))
+        (map (fn [i] [r (op c i)]))
+        (drop-while (fn [loc] (?digit (locmap loc))))
         first
         second)
    n))
 
-(defn part-number [locmap n [r c]]
-  (let [sc (non-digit locmap r c - 0)
-        ec (non-digit locmap r c + n)
+(defn part<-digit [locmap n [r c]]
+  (let [sc (find-non-digit locmap r c - 0)
+        ec (find-non-digit locmap r c + n)
         dig's (for [c (range (inc sc) ec)] (locmap [r c]))]
     [[r (inc sc)] (parse-long (apply str dig's))]))
 
-(defn part's [locmap col's loc]
+(defn part's [locmap cols loc]
   (into {}
-        (map #(part-number locmap col's %))
+        (map #(part<-digit locmap cols %))
         (adjacent-digits locmap loc)))
 
 (defn schematic<-
@@ -44,10 +41,10 @@
    {:v <symbol> :parts <adjacent-part-numbers>}.
 
    The part numbers are also a map, keyed by the loc of the part number's first
-   digit. That loc isn't really important, it just provides a unique key,
-   since part numbers themselves may not be unique(?).
+   digit. That loc isn't really important, it just provides a unique key, and it
+   eliminates duplicates as we check the digits adjacent to a symbol.
 
-   For the sample data, the parsed schematic is:
+   For example, with the sample data, the parsed schematic is:
    [{:v *, :parts {[4 0] 617}}
     {:v $, :parts {[9 1] 664}}
     {:v *, :parts {[0 0] 467, [2 2] 35}}
@@ -57,10 +54,12 @@
 
    With this result in hand, it is easy to complete the two puzzles."
   [input]
-  (let [{:keys [locmap] [_row's col's] :size} (locmap<- (slurp input))]
+  (let [{:keys [locmap] [_rows cols] :size} (locmap<- (slurp input))]
     (into []
-          (map (fn [[k v]] {:v v :parts (part's locmap col's k)}))
-          (symbols<- locmap))))
+          (comp
+           (filter #(?symbol (second %)))
+           (map (fn [[k v]] {:v v :parts (part's locmap cols k)})))
+          locmap)))
 
 (comment
   (def schematic (schematic<- "input/2023/03-schematic.txt"))
