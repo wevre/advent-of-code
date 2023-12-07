07fc3210e9e457355a2e1eccc46c7d68993a465f
wevre <6877948+wevre@users.noreply.github.com>
2023-12-07 09:40:38 -0700

    solve fn, single comparator

--- src/advent_of_code/2023/day_07_camel_poker.clj
+++ src/advent_of_code/2023/day_07_camel_poker.clj
@@ -5,29 +5,11 @@
   (let [[h b] (str/split l #" ")]
     [h (parse-long b)]))
 
-(def card-strength {\A 14
-                    \K 13
-                    \Q 12
-                    \J 11
-                    \T 10
-                    \9 9
-                    \8 8
-                    \7 7
-                    \6 6
-                    \5 5
-                    \4 4
-                    \3 3
-                    \2 2})
+(def card-strength {\A 14 \K 13 \Q 12 \J 11 \T 10 \9 9 \8 8 \7 7 \6 6 \5 5 \4 4 \3 3 \2 2})
 
 (def j-card-strength (assoc card-strength \J 1))
 
-(def type-strength {:five 50
-                    :four 40
-                    :house 35
-                    :three 30
-                    :two-pair 25
-                    :one-pair 20
-                    :high-card 10})
+(def type-strength {:five 50 :four 40 :house 35 :three 30 :two-pair 25 :one-pair 20 :high-card 10})
 
 (defn type<- [hand]
   (let [fr's (frequencies hand)
@@ -41,12 +23,6 @@
         #{3 2} :house
         #{3 1} :three
         #{2 1} :two-pair))))
-
-(defn comp-hand [ha hb]
-  (let [c (compare (type-strength (type<- ha)) (type-strength (type<- hb)))]
-    (if (not= c 0)
-      c
-      (compare (mapv card-strength ha) (mapv card-strength hb)))))
 
 (defn j-type<- [hand]
   (let [fr's (frequencies hand)
@@ -64,30 +40,29 @@
                  1 :house
                  2 :four)))))
 
-(defn j-comp-hand [ha hb]
-  (let [c (compare (type-strength (j-type<- ha)) (type-strength (j-type<- hb)))]
-    (if (not= c 0)
-      c
-      (compare (mapv j-card-strength ha) (mapv j-card-strength hb)))))
+(defn comp-hand [type-er strength-er]
+  (fn [ha hb]
+    (let [c (compare (type-strength (type-er ha)) (type-strength (type-er hb)))]
+      (if (not= c 0)
+        c
+        (compare (mapv strength-er ha) (mapv strength-er hb))))))
+
+(defn solve [input comp]
+  (->> input
+       str/split-lines
+       (map parse-line)
+       (sort-by first comp)
+       (map-indexed vector)
+       (map (fn [[rank [_ bid]]] (* (inc rank) bid)))
+       (reduce +)))
 
 (comment
   (def input (slurp "input/2023/07-sample.txt"))
   (def input (slurp "input/2023/07-camel-poker.txt"))
 
   ;; year 2023 day 07 puzzle 1
-  (->> input
-       str/split-lines
-       (map parse-line)
-       (sort-by first comp-hand)
-       (map-indexed vector)
-       (map (fn [[rank [_ bid]]] (* (inc rank) bid)))
-       (reduce +))
+  (solve input (comp-hand type<- card-strength))   ;; => 251136060
 
   ;; year 2023 day 07 puzzle 2
-  (->> input
-       str/split-lines
-       (map parse-line)
-       (sort-by first j-comp-hand)
-       (map-indexed vector)
-       (map (fn [[rank [_ bid]]] (* (inc rank) bid)))
-       (reduce +)))
+  (solve input (comp-hand j-type<- j-card-strength))   ;; => 249400220
+)
