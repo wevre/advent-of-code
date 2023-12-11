4de97010796a14c4681bf1eb97d86b79a8e08ccd
wevre <6877948+wevre@users.noreply.github.com>
2023-12-11 00:42:00 -0500

    day 11

---
+++ src/advent_of_code/2023/day_11_galaxies.clj
@@ -0,0 +1,42 @@
+(ns advent-of-code.2023.day-11-galaxies
+  (:require [advent-of-code.common :as common]
+            [clojure.math.combinatorics :as combo]))
+
+(defn count-gaps [a b empty's]
+  (let [[a b] (sort [a b])]
+    (->> empty's (filter #(<= a % b)) count)))
+
+(defn path-dist [[ra ca] [rb cb] empty-rows empty-cols factor]
+  (+ (abs (- rb ra))
+     (abs (- cb ca))
+     (* factor (count-gaps rb ra empty-rows))
+     (* factor (count-gaps cb ca empty-cols))))
+
+(defn parse [input]
+  (let [{:keys [locmap size]} (common/locmap<- input)
+        galaxies (->> locmap (keep (fn [[loc c]] (when (= c \#) loc))))
+        galaxy-rows (->> galaxies (map first) set)
+        galaxy-cols (->> galaxies (map second) set)
+        empty-rows (->> (range (first size)) (remove galaxy-rows) sort)
+        empty-cols (->> (range (second size)) (remove galaxy-cols) sort)]
+    {:galaxies galaxies
+     :empty-rows empty-rows
+     :empty-cols empty-cols}))
+
+(defn solve [{:keys [galaxies empty-rows empty-cols]} expand]
+  (transduce (map (fn [[a b]] (path-dist a b empty-rows empty-cols expand)))
+             +
+             (combo/combinations galaxies 2)))
+
+(comment
+  (def input (parse (slurp "input/2023/11-galaxies.txt")))
+  (def input (parse "...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....\n"))
+
+  ;; year 2023 day 11 puzzle 1
+  (solve input 1)
+  ;; => 9769724
+
+  ;; year 2023 day 11 puzzle 2
+  (solve input 999999)
+  ;; => 603020563700
+  )
