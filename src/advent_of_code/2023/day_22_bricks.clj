27e56a7d7669f79c964ae5354638a5b75df74428
wevre <6877948+wevre@users.noreply.github.com>
2023-12-22 06:51:59 -0700

    cleanup

--- src/advent_of_code/2023/day_22_bricks.clj
+++ src/advent_of_code/2023/day_22_bricks.clj
@@ -10,8 +10,10 @@
 
 (defn brick<- [[i [a b]]]
   (let [cubes (sort-by #(nth % 2) (points-along a b))
-        [_ _ min-z] (first cubes)]
-    {:id i :cubes cubes :min-z min-z}))
+        [_ _ min-z] (first cubes)
+        cubes (map (fn [[x y z]] [x y (- z min-z)]) cubes)
+        footprint (keep (fn [[x y z]] (when (zero? z) [x y])) cubes)]
+    {:id i :cubes cubes :z-loc min-z :footprint footprint}))
 
 (defn parse [input]
   (->> input
@@ -19,66 +21,66 @@
        (partition 3)
        (partition 2)
        (map-indexed vector)
-       (map brick<-)))
+       (map brick<-)
+       (sort-by :z-loc)))
 
-;; --- Letting bricks come to rest ---
+;; --- Working with bricks ---
 
-(defn drop-bricks [bricks]
-  (loop [[{:as b :keys [cubes min-z id]} & b's] (sort-by :min-z bricks) tops {}
-         supports {} rests-on {}]
+(defn get-stack [bricks]
+  (loop [[{:as b :keys [cubes id footprint]} & b's] bricks tops {} stack {}]
     (if-not b
-      {:supports supports :rests-on rests-on}
-      (let [footprint (keep (fn [[x y z]] (when (= z min-z) [x y])) cubes)
-            [z lands-on] (->> footprint
-                              (map #(tops % [0 :ground]))
+      stack
+      (let [[z lands-on] (->> footprint
+                              (map #(tops % [0 nil]))
                               (group-by first)
                               (sort-by first >)
                               first)
-            new-pos (map #(map + [0 0 (- (inc z) min-z)] %) cubes)]
+            lands-on (keep second lands-on)
+            new-loc's (map #(map + [0 0 (inc z)] %) cubes)]
         (recur b's
-               (into tops (map (fn [[x y z]] [[x y] [z id]]) new-pos))
-               (reduce (fn [acc [_ b]] (update acc b (fnil conj #{}) id))
-                       supports
-                       lands-on)
-               (reduce (fn [acc [_ b]] (update acc id (fnil conj #{}) b))
-                       rests-on
+               (into tops (map (fn [[x y z]] [[x y] [z id]]) new-loc's))
+               (reduce (fn [acc b]
+                         (-> acc
+                             (update-in [id :rests-on] (fnil conj #{}) b)
+                             (update-in [b :supports] (fnil conj #{}) id)))
+                       stack
                        lands-on))))))
 
+(defn other-supporters [stack id]
+  (->> (get-in stack [id :supports])
+       (map (juxt identity #(get-in stack [% :rests-on])))))
+
+(defn remove-brick [stack]
+  (fn [id]
+    (loop [queue (common/queue id) n 0 goners #{}]
+      (let [[b queue] ((juxt peek pop) queue)]
+        (if-not b
+          (dec n)
+          (let [goners (conj goners b)]
+            (recur (into queue (->> (other-supporters stack b)
+                                    (keep (fn [[a b]]
+                                            (when (empty? (remove goners b)) a)))))
+                   (inc n)
+                   goners)))))))
+
 (comment
-  (def bricks (parse (slurp "input/2023/22-sample.txt")))
-  (def bricks (parse (slurp "input/2023/22-bricks.txt")))
+  (do
+    (def bricks (parse (slurp "input/2023/22-sample.txt")))
+    (def stack (get-stack bricks)))
+  (do
+    (def bricks (parse (slurp "input/2023/22-bricks.txt")))
+    (def stack (get-stack bricks)))
 
   ;; year 2023 day 22 puzzle 1
-  (let [{:keys [supports rests-on]} (drop-bricks bricks)]
-    (reduce (fn [n {:keys [id]}]
-              (if (every? #(< 1 (count %)) (map rests-on (supports id)))
-                (inc n)
-                n))
-            0
-            bricks))
+  (reduce (fn [n {:keys [id]}]
+            (if (every? #(< 1 %) (->> (other-supporters stack id) (map second) (map count)))
+              (inc n)
+              n))
+          0
+          bricks)
   ;; => 473
-  )
 
-;; --- Chain reaction ---
-
-(defn remove-brick [supports rests-on id]
-  (loop [queue (common/queue id) n 0 goners #{}]
-    (let [[b queue] ((juxt peek pop) queue)]
-      (if-not b
-        n
-        (let [goners (conj goners b)]
-          (recur (into queue (filter #(empty? (remove goners (rests-on %))) (supports b)))
-                 (inc n)
-                 goners))))))
-
-(comment
   ;; year 2023 day 22 puzzle 2
-  (let [{:keys [supports rests-on]} (drop-bricks bricks)]
-    (->> bricks
-         (map :id)
-         (map #(remove-brick supports rests-on %))
-         (map dec)
-         (reduce +)))
+  (transduce (comp (map :id) (map (remove-brick stack))) + bricks)
   ;; => 61045
-
   )
